91ff7016117f88d0f19c9f7a86276a756fa31b45
Tendy Hermansyah <55569231+TendyHemansyah@users.noreply.github.com>
2019-09-26 11:25:24 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan9/Teks.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan9;
+
+/**
+ *
+ * @author Tendy
+ * NAMA     : Tendy Hermansyah
+ * KELAS    : PBO2
+ * NIM      : 10118083
+ * Deskripsi Program : Teks
+ */
+public class Teks {
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c= 'A';
+        System.out.println("Hasil" + newline + "Char:" + tab + "berisi " + c);
+    }
+}
